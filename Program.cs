368c7173352f492d9b79ffc788d13cfb6b7f53b9
Italo.Neves <Magnavox789@github.com>
2020-05-19 01:04:20 -0300

    criação do primeiro exemplo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGit
+{
+    
+    class Program
+    {
+        static void Escrevetela()
+        {
+            Console.WriteLine("Primeiro programa com git");
+        }
+        static void Main(string[] args)
+        {
+            Escrevetela();
+            Console.ReadKey();
+        }
+    }
+}
